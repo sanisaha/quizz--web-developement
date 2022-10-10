fffed0aa662e770e283a58e102deffdf928c599a
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-10-10 20:43:04 +0300

    react router,tailwind and daisyui installed

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h4 className='text-lg'>This is header</h4>
+            <button className='btn w-64 rounded-full'>Button</button>
+        </div>
+    );
+};
+
+export default Header;
