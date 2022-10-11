699214215aec7dd3f2a499b2b9eae25e93eebfd3
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-10-11 23:07:04 +0300

    quiz page and dynamic route to quiz page

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -4,15 +4,17 @@ const Question = (props) => {
     const { id, question, options } = props.question;
     return (
         <div className='p-5'>
-            <div>
-                <h2>{question}</h2>
-            </div>
-            <div className='grid grid-cols-2'>
-                {
-                    options.map(option => <div>
-                        <li className='p-5'>{option}</li>
-                    </div>)
-                }
+            <div className='bg-slate-200'>
+                <div className='border border-indigo-300 text-center p-5 font-semibold'>
+                    <h2>{question}</h2>
+                </div>
+                <div className='grid grid-cols-2 gap-2'>
+                    {
+                        options.map(option => <div className='border border-indigo-300'>
+                            <li className='p-5'>{option}</li>
+                        </div>)
+                    }
+                </div>
             </div>
         </div>
     );
--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -8,14 +8,16 @@ const Test = () => {
     return (
         <div>
             <div>
-                <h1 className='text-3xl text-center font-bold'>Quiz from <span className='text-4xl'>{data.name}</span></h1>
+                <h1 className='text-3xl text-center font-bold mb-10'>Quiz from <span className='text-4xl'>{data.name}</span></h1>
             </div>
-            {
-                data.questions.map(question => <Question
-                    key={question.id}
-                    question={question}
-                ></Question>)
-            }
+            <div className='mx-20'>
+                {
+                    data.questions.map(question => <Question
+                        key={question.id}
+                        question={question}
+                    ></Question>)
+                }
+            </div>
             <div>
 
             </div>
